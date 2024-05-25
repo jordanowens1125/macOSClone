abd5a27dbcc2ade95a7495c253e1cd86539ee1ba
Jordan Owens <jordanowens1125@gmail.com>
2024-05-24 20:25:39 -0500

    package.json fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-=======
-  output: "exports", // lightweight but no dynamic routes & api support
-  // output: "standalone", // bulkier but supports dynamic routes & api
->>>>>>> parent of 1129041 (Updates)
 };
 
 export default nextConfig;

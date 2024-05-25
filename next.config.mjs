11290418c80708fab3c793a908d2a0c0a003b43e
Jordan Owens <jordanowens1125@gmail.com>
2024-05-24 19:21:33 -0500

    Updates

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "exports", // lightweight but no dynamic routes & api support
-  // output: "standalone", // bulkier but supports dynamic routes & api
-};
-
-export default nextConfig;
--- src/app/icons/percentage.tsx
+++
@@ -1,8 +0,0 @@
-const PercentageSvgComponent = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-    <g fill="currentColor">
-      <path d="M7.05 17.7a.74.74 0 0 1-.53-.22.75.75 0 0 1 0-1.06l9.9-9.9a.75.75 0 1 1 1.06 1.06l-9.9 9.9a.74.74 0 0 1-.53.22ZM8.5 10.75a2.25 2.25 0 1 1 0-4.5 2.25 2.25 0 0 1 0 4.5Zm0-3a.75.75 0 1 0 .75.75.76.76 0 0 0-.75-.75ZM15.5 17.75a2.25 2.25 0 1 1 0-4.5 2.25 2.25 0 0 1 0 4.5Zm0-3a.75.75 0 1 0 .75.75.76.76 0 0 0-.75-.75Z" />
-    </g>
-  </svg>
-);
-export default PercentageSvgComponent;
